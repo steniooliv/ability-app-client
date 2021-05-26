05dbfe3dd96489934fb63fe20d3ccb4058e0f76d
Stenio Oliveira <steniooliv@gmail.com>
2021-05-26 11:17:38 -0300

    create logout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, {Component} from 'react';
 import Login from './auth/Login';
 import Registration from './auth/Registration';
@@ -7,11 +8,22 @@ export default class Home extends Component {
     super(props);
 
     this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
   }
 
   handleSuccessfulAuth(data) {
     this.props.handleLogin(data);
     this.props.history.push("/dashboard");
+  }
+
+  handleLogoutClick() {
+    axios
+      .delete("http://172.19.47.8:3001/logout", {withCredentials: true})
+      .then(response =>{
+        this.props.handleLogout()
+      })
+      .catch(err => console.log(err)
+    );
   }
   
   render (){
@@ -19,6 +31,7 @@ export default class Home extends Component {
       <div>
         <h1>Home</h1>
         <h1>Status: {this.props.loggedInStatus}</h1>
+        <button onClick={() => this.handleLogoutClick()}>Logout</button>
         <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
         <Login handleSuccessfulAuth={this.handleSuccessfulAuth} />
       </div>
--- src/components/app.js
+++ src/components/app.js
@@ -15,6 +15,7 @@ export default class App extends Component {
     };
 
     this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
   }
 
   checkLoginStatus(){
@@ -46,6 +47,13 @@ export default class App extends Component {
   componentDidMount(){
     this.checkLoginStatus();
   }
+  
+  handleLogout(data) {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
+    });
+  }
 
   handleLogin(data) {
     this.setState({
@@ -63,14 +71,22 @@ export default class App extends Component {
               exact
               path={'/'}
               render={props => (
-                <Home {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.loggedInStatus} />
+                <Home
+                  {...props}
+                  handleLogin={this.handleLogin}
+                  handleLogout={this.handleLogout}
+                  loggedInStatus={this.state.loggedInStatus}
+                />
               )}
             />
             <Route
               exact
               path={'/dashboard'}
               render={props => (
-                <Dashboard {...props} loggedInStatus={this.state.loggedInStatus} />
+                <Dashboard
+                  {...props}
+                  loggedInStatus={this.state.loggedInStatus}
+                />
               )}
             />
           </Switch>
